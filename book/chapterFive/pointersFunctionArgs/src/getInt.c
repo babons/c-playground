398091c048d211ded90cd7d45b249583e6787970
babons <sebastianbabon213@gmail.com>
2025-06-25 23:25:27 -0400

    added some stuff

--- book/chapterFive/pointersFunctionArgs/src/getInt.c
+++ book/chapterFive/pointersFunctionArgs/src/getInt.c
@@ -8,6 +8,7 @@ int bufp = 0;
 int getch(void);
 void ungetch(int);
 int getint(int *pn);
+float getfloat(float *fp);
 
 /*
 getint: gets next integer from input and stores ino *pn
@@ -16,11 +17,70 @@ getint: gets next integer from input and stores ino *pn
 int main()
 {
 	int n;
+	float f;
 	for (;;) {
-		printf("Enter input: \n");
+		printf("Enter input: ");
 		getint(&n);
 		printf("Input: %d\n", n);
+		printf("Enter float input: ");
+		getfloat(&f);
+		printf("Float Input: %f\n", f);
+	}
 	return 0;
+}
+
+float getfloat(float *fp)
+{
+	int c, p, sign, temp;
+	while (isspace(c = getch()))
+		;
+	if (!isdigit(c) && c != EOF && c != '+' && c != '-' && c != '.') {
+		ungetch(c);
+		return 0;
+	}
+
+	sign = (c == '-') ? -1 : 1;
+	if (c == '+' || c == '-') {
+                temp = getch();
+                if (!isdigit(temp) && temp != '.') {
+                        ungetch(temp);
+                        ungetch(c);
+                        return 0;
+                } else {
+                        ungetch(temp);
+                        c = getch();
+                }
+        }
+
+	if (c == '.') {
+		temp = getch();
+		if (!isdigit(temp)) {
+			ungetch(temp);
+			ungetch(c);
+			return 0;
+		} else {
+			ungetch(temp);
+			c = getch();
+		}
+	}
+	p = 1;
+	for (*fp = 0; isdigit(c); c = getch()) {
+		*fp = 10 * *fp + (c - '0');
+		p *= 10;
+	}
+	if (c == '.') {
+		double frac = 0.0;
+		p = 1;
+		while (isdigit(c = getch())) {
+			frac = 10 * frac + (c - '0');
+			p *= 10;
+		}
+		*fp += frac / p;
+	}
+	*fp *= sign;
+	if (c != EOF)
+		ungetch(c);
+	return c;
 }
 
 int getint(int *pn)
