709adb4947111afb00ef5c8d40d644d6da49b6aa
babons <sebastianbabon213@gmail.com>
2025-06-29 02:32:42 -0400

    tidying up

--- arrayMemAdd/memChange.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	int arr[5] = {1, 2, 3, 4, 5};
-	for (int i = 0; i < 5; i++) {
-		printf("arr[%d] = %d, address = %p\n", i, arr[i], (void*)&arr[i]);
-	}
-	return 0;
-}
--- bitwise/src/andDemo.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	int n = 254;
-	printf("%d\n", n & 0177); // sets all but the low-order 7 bits to 0
-	return 0;
-}
