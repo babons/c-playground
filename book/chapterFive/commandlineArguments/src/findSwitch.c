ede7578f19062fc513e1e7b521f0e93cfad9a933
babons <sebastianbabon213@gmail.com>
2025-07-06 13:06:10 -0400

    Revert "working findSwitch (?)"
    This reverts commit bda2f1b65f4053956ef37d12ddd1e0a92768f5d4.

--- book/chapterFive/commandlineArguments/src/findSwitch.c
+++ book/chapterFive/commandlineArguments/src/findSwitch.c
@@ -48,17 +48,9 @@ int my_getline(char *s, int lim)
 {
         int i = 0;
         int c;
-
-        while ((c = getchar()) && c != EOF && i < lim) {
-                if (c == '\n') {
-			s[i++] = c;
-			break;
-		}
-		s[i++] = c;
+        while ((c = getchar()) && c != EOF && c != '\n' && c != '#' && i < lim) {
+                s[i++] = c;
         }
-
-	s[i] = '\0';
-	return i;
 
         s[i] = '\0';
         return i;
