27447cb86ab32bea1614e82d02aa4eced2ee0b90
babons <sebastianbabon213@gmail.com>
2025-07-09 23:55:11 -0400

    removed redundant program

--- gptprojs/tinyVM/src/prototype.c
+++ gptprojs/tinyVM/src/prototype.c
@@ -13,12 +13,6 @@ int pc = 0;
 int main()
 {
 	void (*ptr[]) (int) = {halt, load, add, print};
-	int programOne[][2] = {
-		{LOAD, 6},
-		{ADD, 3},
-		{PRINT, 0},
-		{HALT, 0}
-	};
 
 	while (state == 1) {
 		ptr[programOne[pc][0]](programOne[pc][1]);
