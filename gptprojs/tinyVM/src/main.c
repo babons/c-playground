8091ca75fc3a3f429d0ffaa0c7ef1f7b7f2f5329
babons <sebastianbabon213@gmail.com>
2025-07-13 17:40:51 -0400

    added tracing switch. todo: print opcodes in trace as string

--- gptprojs/tinyVM/src/main.c
+++ gptprojs/tinyVM/src/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include "ctype.h"
 #include "operations.h"
 #include "opcodes.h"
 #include "programs.h"
@@ -8,10 +10,19 @@
 High-level prototype for this project.
 */
 
-int main()
+int main(int argc, char *argv[])
 {
+	// argument handling
+	int trace = 0;
+	//if (argc > 1) {
+	//	printf("too many arguments\n");
+	//}
+	if (argc == 2 && strcmp(argv[1], "-t") == 0) {
+		printf("OPCODES: 0 HALT 1 LOAD 2 ADD 3 SUB 4 PRINT\n\n");
+		trace = 1;
+	}
 	printf("Program One output:\n");
-	vmrun(programOne);
+	vmrun(programOne, trace);
 	printf("\nProgram Two output:\n");
-	vmrun(programTwo);
+	vmrun(programTwo, trace);
 }
--- gptprojs/tinyVM/src/vm.c
+++ gptprojs/tinyVM/src/vm.c
@@ -6,15 +6,28 @@
 int state = 1;
 int acc = 0;
 int pc = 0;
+int trace = 0;
 
-void vmrun(int program[][2])
+void vmrun(int program[][2], int trace)
 {
 	state = 1;
 	acc = 0;
 	pc = 0;
 	void (*ptr[])(int) = {halt, load, add, sub, print};
 	while (state) {
-		ptr[program[pc][0]](program[pc][1]);
-                pc++;
+		if (trace == 1) {
+			if ((program[pc][0]) != 0) {
+				printf("r: %d\n%d: %d, %d\n\n",
+					acc, pc + 1, program[pc][0], program[pc][1]);
+				ptr[program[pc][0]](program[pc][1]);
+				pc++;
+			} else {
+				ptr[program[pc][0]](program[pc][1]);
+				printf("Halt!\n");
+			}
+		} else {
+			ptr[program[pc][0]](program[pc][1]);
+                	pc++;
+		}
 	}
 }
