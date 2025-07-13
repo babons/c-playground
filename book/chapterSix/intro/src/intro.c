8e7dd4c3e59a722fa73d0ef908955750c114baf8
babons <sebastianbabon213@gmail.com>
2025-07-13 19:24:38 -0400

    struct functions

--- book/chapterSix/intro/src/intro.c
+++ book/chapterSix/intro/src/intro.c
@@ -4,13 +4,37 @@
 Demo structures and stuff
 */
 
+struct point {
+	int x, y;
+};
+
+struct point make_point(int x, int y);
+
 int main()
 {
-	struct point {
-		int x,y;
-	};
+	//!!! defining and using a simple struct
+//	struct point {
+//		int x,y;
+//	};
 
 	struct point p = {3, 4};
 
-	printf("%d, %d\n", p.x, p.y);
+	// printf("%d, %d\n", p.x, p.y);
+
+	//!!! struct pointers
+	struct point *pp = &p;
+
+	//printf("%d, %d\n", pp->x, pp->y);
+
+	//!! struct functions
+	struct point a = make_point(2, 5);
+	printf("%d, %d\n", a.x, a.y);
+}
+
+struct point make_point(int x, int y)
+{
+	struct point p;
+	p.x = x;
+	p.y = y;
+	return p;
 }
