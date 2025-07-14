b8c9e667c42afb0604c6022ee9f637084ca1f777
babons <sebastianbabon213@gmail.com>
2025-07-14 06:12:45 -0400

    string structs + string struct manip

--- book/chapterSix/structArrays/src/structLevelThree.c
+++ book/chapterSix/structArrays/src/structLevelThree.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #define NPOINTS (sizeof points / sizeof(struct point))
+#define NKEYS (sizeof keys / sizeof (struct key))
+
 struct point {
 	int x,y;
 } points[] = {
@@ -10,10 +12,30 @@ struct point {
 	9,10
 };
 
+struct key {
+	char* word;
+	int count;
+} keys[] = {
+	"clear",0,
+	"write",0,
+	"word",0,
+	"meow",0,
+	"memory",0,
+};
+
 void shiftpoints(struct point points[], int n, int dx, int dy)
 {
 	points[n].x += dx;
 	points[n].y += dy;
+}
+
+void incwordcount(struct key keys[], int n, char *target)
+{
+	for (int i = 0; i < n; i++) {
+		if (keys[i].word == target) {
+			keys[i].count++;
+		}
+	}
 }
 
 void printpoints()
@@ -21,6 +43,14 @@ void printpoints()
         for (int i = 0; i < NPOINTS; i++) {
                 printf("%d, %d\n", points[i].x, points[i].y);
         }
+	printf("\n");
+}
+
+void printcounts()
+{
+	for (int i = 0; i < NKEYS; i++) {
+		printf("%s, %d\n", keys[i].word, keys[i].count);
+	}
 	printf("\n");
 }
 
@@ -32,5 +62,9 @@ int main()
 	shiftpoints(points, 0, 10, 10);
 	printpoints();
 
+	// String array / String array manip
+	printcounts();
+	incwordcount(keys, NKEYS, "clear");
+	printcounts();
 	return 0;
 }
